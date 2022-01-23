2c09e2c8f95e8e9893ade343bbed6c69342ee365
Arsalan <50211547+Azzy001@users.noreply.github.com>
2022-01-23 12:53:15 +0000

    Update Sales_GUI.py
    I have added frames to my program, My plan is to add the buttons, entries and labels widgets inside the frames

--- Sales_GUI.py
+++ Sales_GUI.py
@@ -6,6 +6,18 @@ window = Tk()
 header = Label(window, text="Vehicle Sales Management System")
 header.place(x=15, y=28)
 
+# Left Frame
+LeftF = Frame(window, width=620, height=350, highlightbackground="lightgrey", highlightthickness=0.5, bd=2)
+LeftF.place(x=15, y=50)
+
+# Right Frame
+RightF = Frame(window, width=285, heigh=435, highlightbackground="lightgrey", highlightthickness=0.5, bd=2, bg="white")
+RightF.place(x=650, y= 50)
+
+# Bottom Frame
+BottomF = Frame(window, width=620, heigh=70, highlightbackground="lightgrey", highlightthickness=0.5, bd=2)
+BottomF.place(x=15, y=415)
+
 # window configurations
 window.geometry("950x500")
 window.resizable(0, 0)
